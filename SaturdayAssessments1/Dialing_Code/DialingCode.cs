4d8588e1de0f3892f5402c82eab99f21e351b36a
Ritikkumar993 <ritiksah99344@gmail.com>
2026-01-10 17:11:07 +0530

    final fixes

--- SaturdayAssessments1/Dialing_Code/DialingCode.cs
+++ SaturdayAssessments1/Dialing_Code/DialingCode.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 
 namespace DialingCodesApp
 {
+   
     public static class DialingCodes
     {
-        static Dictionary<int, string> reg = new Dictionary<int, string>();
+        static Dictionary<int, string> reg=new Dictionary<int, string>();
 
         public static Dictionary<int, string> GetEmptyDictionary()
         {
-            Dictionary<int, string> dic = new Dictionary<int, string>();
+            Dictionary<int, string> dic=new Dictionary<int, string>();
             return dic;
-        }
+        }    
 
         public static Dictionary<int, string> GetExistingDirectory()
         {
@@ -21,74 +22,55 @@ namespace DialingCodesApp
         public static Dictionary<int, string> AddCountryToEmptyDictonary(int CountryCode, string CountryName)
         {
             Dictionary<int, string> dic = new Dictionary<int, string>();
-            dic.Add(CountryCode, CountryName);
+            dic.Add(CountryCode,CountryName);
+
             return dic;
         }
 
-        public static Dictionary<int, string> AddCountryToExistingDictionary(
-            Dictionary<int, string> existingDictionary,
-            int countryCode,
-            string countryName)
+        public static Dictionary<int, string> AddCountryToExistingDictionary(Dictionary<int,string> existingDictionary,int countryCode, string countryName)
         {
-            existingDictionary[countryCode] = countryName;
+            existingDictionary.Add(countryCode,countryName);
             return existingDictionary;
         }
 
-        public static string GetCountryNameFromDictionary(
-            Dictionary<int, string> existingDictionary,
-            int CountryCode)
+        public static string GetCountryNameFromDictionary(Dictionary<int, string> existingDictionary,int CountryCode)
         {
-            if (existingDictionary.ContainsKey(CountryCode))
-            {
+            if(existingDictionary.ContainsKey(CountryCode))    
                 return existingDictionary[CountryCode];
-            }
             return "";
         }
-
-        public static bool CheckCodeExists(
-            Dictionary<int, string> existingDictionary,
-            int countryCode)
+        public static bool CheckCodeExists(Dictionary<int, string> existingDictionary,int countryCode)
         {
             return existingDictionary.ContainsKey(countryCode);
         }
 
-        public static Dictionary<int, string> UpdateDictonary(
-            Dictionary<int, string> existingDictionary,
-            int countryCode,
-            string countryName)
+        public static Dictionary<int,string> UpdateDictonary(Dictionary<int, string>existingDictionary, int countryCode, string countryName)
         {
             if (existingDictionary.ContainsKey(countryCode))
             {
-                existingDictionary[countryCode] = countryName;
+                existingDictionary[countryCode]=countryName;
             }
             return existingDictionary;
         }
 
-        public static Dictionary<int, string> RemoveCountryFromDictionary(
-            Dictionary<int, string> existingDictionary,
-            int countryCode)
-        {
-            if (existingDictionary.ContainsKey(countryCode))
-            {
+        public static Dictionary<int, string> RemoveCountryFromDictionary(Dictionary<int, string>existingDictionary,int countryCode){
+            if(existingDictionary.ContainsKey(countryCode))
                 existingDictionary.Remove(countryCode);
-            }
             return existingDictionary;
         }
 
-        public static string FindLongestCountryName(
-            Dictionary<int, string> existingDictionary)
+        public static string FindLongestCountryName(Dictionary<int, string>existingDictionary)
         {
-            string res = "";
-            foreach (var it in existingDictionary)
+            string res="";
+            foreach(var it in existingDictionary)
             {
                 if (it.Value.Length > res.Length)
                 {
-                    res = it.Value;
+                    res=it.Value;
                 }
             }
             return res;
         }
-    }
-
     
+    }
 }
