b225410a3e30400ce374c81a003fe54d281169f3
Ritikkumar993 <ritiksah99344@gmail.com>
2026-01-13 16:23:55 +0530

    Assembly & Reflection AND Assessment

---
+++ Day-16-Assembly-Reflection/Day-16/Program.cs
@@ -0,0 +1,139 @@
+﻿// using System.Reflection;
+
+
+// class Employee
+// {
+//     public string Name{get; set;}
+//     private int _Id;
+//     public Employee(string name, int id)
+//     {
+//         Name=name;
+//         _Id=id;
+//         Console.WriteLine("Name: "+Name+"\n"+"Id"+_Id);
+//     }
+//     public void Work(string greet, string k)
+//     {
+//         Console.WriteLine("Working "+greet+" "+k);
+//     }
+    
+// }
+// class Program
+// {
+//     static void Main()
+//     {
+        
+//         // Assembly assembly = Assembly.GetExecutingAssembly();
+//         // Console.WriteLine(assembly);
+
+//         // Console.WriteLine(Assembly.Load("Day-16"));
+//         // Console.WriteLine(Assembly.LoadFrom("K:/csharp/dot_net/DigitalWallet/DigitalWallet/bin/Debug/net10.0/DigitalWallet.dll"));
+
+//         // Employee emp= new Employee("Ritik", 191);
+//         // Console.WriteLine(emp.GetType()); 
+
+//         // Type type  = typeof(Employee);
+//         // Console.WriteLine(type);
+
+//         // Type type2 = emp.GetType();
+//         // Console.WriteLine(type2);
+
+//         // Type type3 = Type.GetType("Day-16.Models.Employee");
+//         // Console.WriteLine(type3);
+
+//         // MethodInfo method = type.GetMethod("Work");
+//         // object[] ob =new object[]{"Good","kk"};
+//         // method.Invoke(emp,ob);
+
+//         // PropertyInfo prop = type.GetProperty("Name");
+//         // prop.SetValue(emp, "John");
+//         // Console.WriteLine(emp.Name);
+
+//         // FieldInfo field = type.GetField(
+//         //     "_Id",
+//         //     BindingFlags.NonPublic | BindingFlags.Instance
+
+//         // );
+//         // Console.WriteLine(field);
+
+//         // Console.WriteLine(field.GetValue(emp));
+//         // field.SetValue(emp, 190);
+//         // Console.WriteLine(field.GetValue(emp));
+        
+//         // Console.WriteLine("ConstructorInfo ");
+
+//         // // ConstructorInfo ctor = type.GetConstructor(Type.EmptyTypes);
+//         // // object obj =ctor.Invoke(null);
+//         // // obj.Name="Ritik";
+//         // // Console.WriteLine(obj.Name);
+
+//         // ConstructorInfo ctor = type.GetConstructor(
+//         //     new Type[] { typeof(string), typeof(int) }
+//         // );
+//         // object obj = ctor.Invoke(new object[] { "Amit", 101 });
+
+
+//         // Console.WriteLine("ParameterInfo ");
+//         // ParameterInfo[] parameters = method.GetParameters();
+
+//         // Console.WriteLine(parameters.Length);
+
+
+//         // Console.WriteLine("ParameterInfo");
+
+//         // foreach (ParameterInfo p in parameters)
+//         // {
+//         //     Console.WriteLine($"{p.Name} - {p.ParameterType}");
+//         // }
+
+//         Assembly assembly = Assembly.GetExecutingAssembly();
+
+//         foreach (Type type in assembly.GetTypes())
+//         {
+//             Console.WriteLine("Class: " + type.Name);
+
+//             foreach (MethodInfo method in type.GetMethods())
+//             {
+//                 Console.WriteLine("  Method: " + method.Name);
+//             }
+//         }
+//     }
+// }
+
+
+using System;
+using System.Reflection;
+
+class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public void Work()
+    {
+        Console.WriteLine("Employee working");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Type type = typeof(Employee);
+
+        Console.WriteLine("Class Name: " + type.Name);
+        Console.WriteLine("Namespace: " + type.Namespace);
+
+        Console.WriteLine("\nProperties:");
+        foreach (PropertyInfo prop in type.GetProperties())
+        {
+            Console.WriteLine($"{prop.Name} - {prop.PropertyType}");
+        }
+
+        Console.WriteLine("\nMethods:");
+        foreach (MethodInfo method in type.GetMethods())
+        {
+            Console.WriteLine(method.Name);
+        }
+    }
+}
+
---
+++ Day-16-Assembly-Reflection/SdlcUltraEnterprise/Program.cs
@@ -0,0 +1,174 @@
+﻿
+
+namespace UltraEnterpriseSDLC
+{
+    public enum RiskLevel
+    {
+        Low,
+        Medium,
+        High,
+        Critical
+
+    }
+
+    public enum SDLCStage
+    {
+        Backlog,
+        Requirement,
+        Design,
+        Development,
+        CodeReview,
+        Testing,
+        UAT,
+        Deployment,
+        Maintenance
+
+    }
+
+    sealed class Requirement
+    {
+        public readonly int Id{ get;}
+        public readonly string Title{ get;}
+        public readonly RiskLevel Risk{ get; }
+
+        public Requirement(int id, string title, RiskLevel risk)
+        {
+            Id=id;
+            Title=title;
+            Risk=risk;
+        }
+    }
+
+    sealed class WorkItem
+    {
+        public readonly int Id {get;}
+        public readonly string Name {get;}
+        public readonly SDLCStage  Stage {get;}
+
+        public readonly HashSet<int> DependencyIds{get; }
+
+        public WorkItem(int id, string name, SDLCStage stage)
+        {
+            Id=id;
+            Name=name;
+            Stage = stage;
+            DependencyIds=new HashSet<int>();
+        }
+    }
+    sealed class BuildSnapshot
+    {
+        public readonly string Version {get;}
+        public readonly DateTime Timestamp {get;}
+
+        public BuildSnapshot(string version)
+        {
+            Version =version;
+            Timestamp=DateTime.Now;
+        }
+
+        
+    }
+
+    sealed class AuditLog
+    {
+        public DateTime Time{get;}
+        public string Action{get;}
+        public AuditLog(string action)
+        {
+            Time=DateTime.Now;
+            Action=action;
+        }
+
+    }
+
+    sealed class QualityMetric
+    {
+        public string Name {get;}
+        public double Score { get;}
+
+        public QualityMetric(string name, double score)
+        {
+            Name=name;
+            Score=score;
+        }
+    }
+
+    class EnterpriseSDLCEngine
+    {
+        private List<Requirement> _requirements;
+        private Dictionary<int,WorkItem> _workItemRegistry;
+        private SortedDictionary<SDLCStage,WorkItem> _stageBoard;
+        private Queue<WorkItem> _executionQueue;
+        private Stack<BuildSnapshot> _rollbackStack;
+        private HashSet<string> _uniqueTestSuites;
+        private LinkedList<AuditLog> _auditLedger;
+        private SortedList<double, QualityMetric> _releaseScoreboard;
+        private int _requirementCounter;
+        private int _workItemCounter;
+
+        public EnterpriseSDLCEngine()
+        {
+            _requirements=new List<Requirement>();
+            _workItemRegistry = new Dictionary<int,WorkItem>();
+            _stageBoard = new SortedDictionary<SDLCStage, List<WorkItem>>();
+
+            foreach (var stage in SDLCStage)
+            {
+                _stageBoard[stage]=new List<WorkItem>();
+            }
+
+            _executionQueue = new Queue<WorkItem>();
+            _rollbackStack  = new Stack<BuildSnapshot>();
+            _uniqueTestSuites  = new HashSet<string>();
+            _auditLedger  = new LinkedList<AuditLog>();
+            _releaseScoreboard  = new SortedList<double, QualityMetric>();
+        }
+
+        
+        public void AddRequirement(string title, RiskLevel risk)
+        {
+            Requirement req = new Requirement(_requirementCounter,title,risk);
+            _requirementCounter++;
+            _requirements.Add(req);
+
+            AuditLog alog = new AuditLog($" Title {title} RiskLeevel {risk}");
+            _auditLedger.Append(alog);
+        }
+
+        public WorkItem CreateWorkItem(string name, SDLCStage stage)
+        {
+            WorkItem workItem =new WorkItem(_workItemCounter,name, stage);
+            _workItemCounter++;
+            _workItemRegistry[workItem.Id]=workItem;
+
+            _stageBoard[workItem.stage].Add(workItem);
+
+            AuditLog auditLog= new AuditLog($"WorkItem is created {workItem.Name} stage {workItem.Stage} ");
+            _auditLedger.Append(auditLog);
+
+            return workItem;
+        }
+
+        public void AddDependency(int workItemId, int dependsOnId)
+        {
+            if(_workItemRegistry.ContainsKey(workItemId) && _workItemRegistry.ContainsKey(dependsOnId))
+            {
+                WorkItem workItem = _workItemRegistry[workItemId];
+                workItem.DependencyIds.Add(dependsOnId);
+            }
+            AuditLog auditlog = new AuditLog($"workItem {workItemId} is dependds upon {dependsOnId}");
+            _auditLedger.Append(auditlog);
+        }
+
+        public void PlanStage(SDLCStage stage)
+        {
+            List<WorkItem> AllWorkItem = _stageBoard[stage];
+
+            var FilteredWorkItem = AllWorkItem.Where(w => _workItemRegistry.ContainsKey(w.DependencyIds) && );
+               
+        }
+
+
+
+    }
+}
