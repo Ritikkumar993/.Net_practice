620f8019fa83e2fb5fb0c3e82af48fe5075016c7
Ritikkumar993 <ritiksah99344@gmail.com>
2026-01-22 16:04:41 +0530

    Assessment Reflection && Assembly

---
+++ SaturdayAssessments1/pettyCashFile/PettyCash/PettyCash/Class1.cs
@@ -0,0 +1,50 @@
+﻿namespace PettyCash
+{
+    interface I1
+    {
+        public void m1();
+        public void m2();
+
+    }
+    interface I2
+    {
+        public void m3();
+
+    }
+    interface I3
+    {
+        public void m4();
+    }
+    
+    public class ClassA:I1, I2, I3
+    {
+        public void m1()
+        {
+            Console.WriteLine("Method m1 is called");
+        }
+        public void m2()
+        {
+            Console.WriteLine("Method m2 is called");
+        }
+        public void m3()
+        {
+            Console.WriteLine("Method m2 is called");
+        }
+       
+        public void m4()
+        {
+            Console.WriteLine("Method m2 is called");
+        }
+        
+    }
+
+    public class ClassB: ClassA
+    {
+        public void Bm5new()
+        {
+            Console.WriteLine("Method m2 is called");
+        }
+    }
+}
+
+
---
+++ SaturdayAssessments1/pettyCashFile/ReflectionApp/ReflectionApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        string dllpath = @"K:\\csharp\\dot_net\\SaturdayAssessments1\\pettyCashFile\\PettyCash\\PettyCash\\bin\\Debug\\net10.0\\PettyCash.dll";
+
+        Assembly assembly = Assembly.LoadFrom(dllpath);
+
+        Console.WriteLine("ASSEMBLY LOADED");
+        Console.WriteLine("----------------");
+
+        foreach (Type type in assembly.GetTypes())
+        {
+            Console.WriteLine($"Type: {type.Name}");
+
+            if (type.IsInterface)
+                Console.WriteLine("  → This is an INTERFACE");
+
+            if (type.IsClass)
+                Console.WriteLine("  → This is a CLASS");
+            if (type.Name == "ClassB")
+            {
+
+                Console.WriteLine("  Members:");
+                foreach (var member in type.GetMembers())
+                {
+                    Console.WriteLine($"     {member.MemberType} : {member.Name}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
